89821866707965fa890bee3e2a640e73b9ecab9f
BrunoDuranTeodoro <duranteodoro20@gmail.com>
2025-10-02 12:42:42 -0300

    exceptions_service

--- src/main/java/com/br/aweb/sistema_vendas/services/PedidoServices.java
+++ src/main/java/com/br/aweb/sistema_vendas/services/PedidoServices.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.br.aweb.sistema_vendas.model.Cliente;
 import com.br.aweb.sistema_vendas.model.Pedido;
+import com.br.aweb.sistema_vendas.model.StatusPedido;
 import com.br.aweb.sistema_vendas.repository.PedidoRepository;
 import com.br.aweb.sistema_vendas.repository.ProdutoRepository;
 
@@ -26,9 +27,11 @@ public class PedidoServices {
     }
 
 
-    @Transactional
+    @Transactional 
+        //Passando parametro
         public void adicionarItem(Long pedidoId, Long produtoId, Integer quantidade){
-
+        
+        //Busca por id no produto repository se ele está presente, se não, ele gera uma exception
         var optinalProduto = produtoRepository.findById(produtoId);
         if (!optinalProduto.isPresent()){
             throw new IllegalArgumentException("Produto não enontrado");
@@ -38,8 +41,20 @@ public class PedidoServices {
         if (!optinalPedido.isPresent()){
             throw new IllegalArgumentException("Pedido não enontrado");
         }
+  
+        if(!pedido.getStatus().equals(StatusPedido.ATIVO)){
+            throw new IllegalArgumentException("Pedido não está ativo no momento");
+        }
 
-        
+        if(produto.getEstoque() < quantidade){
+            throw new IllegalArgumentException("Quantidade insuficiente do produto" + produto.getNome());
+        }
+
+
+        var pedidoExistente = optinalPedido.get();
+
+
+
     }
         
 
